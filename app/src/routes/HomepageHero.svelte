c2e2925a31dfa5c1ebd0032cc1891a68c64a10b1
Roga <ryanroga@gmail.com>
2023-09-08 14:02:59 -0300

    Update HomepageHero.svelte

--- app/src/routes/HomepageHero.svelte
+++ app/src/routes/HomepageHero.svelte
@@ -58,7 +58,7 @@
 						</p>
 						<div class="mt-10 flex items-center gap-x-6">
 							<a
-								href="/store"
+								href="/shop"
 								class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 								>See our selection</a
 							>
