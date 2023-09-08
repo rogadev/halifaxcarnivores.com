2898ccd6f31d3b84496bf640652a1f505a74be8c
Roga <ryanroga@gmail.com>
2023-09-08 14:00:33 -0300

    Create LinkText.svelte
    New text link component for hyperlinks inside text blocks.

---
+++ app/src/lib/components/LinkText.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let href: string = '';
+	export let target: string | undefined = undefined;
+</script>
+
+<a class="text-blue-700 hover:text-blue-500 hover:underline font-semibold" {href} {target}
+	><slot /></a
+>
