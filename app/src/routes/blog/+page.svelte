38b3e3beb0f2ee07492d4133b3d475f9534add85
Roga <ryanroga@gmail.com>
2023-09-08 13:00:55 -0300

    Roughed Out Pages
    Roughed out About, Blog, and Shop

---
+++ app/src/routes/blog/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import UnderConstruction from '$lib/components/UnderConstruction.svelte';
+</script>
+
+<svelte:head>
+	<title>Blog</title>
+</svelte:head>
+
+<UnderConstruction />
