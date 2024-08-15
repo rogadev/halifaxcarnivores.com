c963d93c531560e6a4574c348fcdbfe1f11494bc
Roga <ryanroga@gmail.com>
2024-08-15 09:46:42 -0300

    feat: Add Events page with under construction message and metadata

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import UnderConstruction from '$lib/components/UnderConstruction.svelte';
+</script>
+
+<svelte:head>
+	<title>
+		Halifax Carnivores Events - Farmers Markets, Workshops, and Educational - Event Bookings
+	</title>
+	<meta
+		name="description"
+		content="Discover where Halifax Carnivores will be next! Join us at local farmers markets, attend our engaging workshops, or book us for your next event. Whether you're interested in learning about exotic carnivorous plants or want to bring a unique educational experience to your classroom, our events page has all the details. Explore upcoming events, workshops, and booking options to bring the fascinating world of carnivorous plants to life."
+	/>
+	<meta
+		name="keywords"
+		content="Halifax carnivorous plant events, exotic plant workshops Halifax, farmers markets carnivorous plants, plant education Halifax, book carnivorous plant workshop, plant event booking, classroom plant presentation, Halifax plant showcase, Nepenthes workshop, Sarracenia education"
+	/>
+</svelte:head>
+
+<UnderConstruction title="Events" />
