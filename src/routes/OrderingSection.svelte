40d37ffbeba85ee8f608e0d6604c1625faa45df9
Roga <ryanroga@gmail.com>
2024-08-15 09:45:35 -0300

    feat: Add OrderingSection component to page

---
+++ src/routes/OrderingSection.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	//
+</script>
+
+<section id="ordering" class="mb-6">
+	<h2 class="text-3xl mb-4 font-bold">Ordering & Pickup</h2>
+	<h3 class="text-2xl mb-4 font-bold">How do I order?</h3>
+	<p class="mb-4">
+		Ordering is easy. Make a list of the items you want. Schedule a pickup time and come get your
+		cool, new plants.
+	</p>
+	<h3 class="text-2xl mb-4 font-bold">Picking up your product.</h3>
+	<p>
+		Orders are available for pickup at your convenience. Once you've chosen your plants, simply
+		visit our <a href="/schedule-pickup" class="link">schedule pickup</a> page and choose a time that
+		works for you, add it to your calendar, and follow the provided instructions. When you arrive, follow
+		your pickup instructions. When we know you're here, we'll bring down our best plants for selection.
+	</p>
+</section>
